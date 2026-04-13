e04d999e79dbd6c083c243dc4dba6dbe489d79fa
kc <kcgbarbosa@gmail.com>
2026-04-13 12:07:19 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        const charMap= {};
+
+        for (let i = 0; i < s.length; i++) {
+            const char = s[i];
+            if (!charMap[char]) {
+                charMap[char] = 1;
+            } else {
+                charMap[char] = charMap[char] + 1;
+            }
+        }
+
+        for (let i = 0; i < t.length; i++) {
+            const char = t[i];
+            if (charMap[char] === undefined || charMap[char] === 0) {
+                return false;
+            }
+            if (charMap[char]) {
+                charMap[char] = charMap[char] - 1
+            }
+        }
+        return true;
+    }
+}
