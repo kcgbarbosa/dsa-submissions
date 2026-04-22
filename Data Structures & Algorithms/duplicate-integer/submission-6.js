b4c2a496a4246ab0a1a3b64bb38d045dfed82c9a
kc <kcgbarbosa@gmail.com>
2026-04-22 10:55:55 -0400

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const checked = new Set();
+
+        for (let i = 0; i < nums.length; i++) {
+            if (checked.has(nums[i])) {
+                return true;
+            } else {
+                checked.add(nums[i])
+            }
+        }
+        return false;
+    }
+}
